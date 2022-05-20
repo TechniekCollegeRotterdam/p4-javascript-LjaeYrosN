eb8e9b858065942b05870df5e5c33d9e39432559
LjaeYrosN <j2drxl@outlook.com>
2022-05-20 12:12:02 +0200

    br

--- les2/app.js
+++ les2/app.js
@@ -1,11 +1,33 @@
-//Oefening angry-birds
-const bird = document.querySelectorAll(".bird");
-let count = 0;
-//var bird en count
-bird.addEventListener("click", function(){
-    let count = count + 5;
-    //count veranderen
-    bird.style.left = count + "px";
-});
-//styling op de bird
+ //Oefening angry-birds
+ const bird = document.querySelectorAll(".bird");
+ let count = 0;
+ //var bird en count
+//  bird.addEventListener("click", function(){
+//      let count = count + 50;
+//      //count veranderen
+//     bird.style.left = count + "px";
+// });
+// //styling op de bird
+  window.addEventListener("keydown", function(e){
+console.log(e);
+if(e.key == "ArrowRight"){
+    count -= 50;
+    bird.style.right = count + "px";
 
+}
+if(e.key == "ArrowUp"){
+    count = count + 50;
+    bird.style.top = count + "px";
+
+}
+if(e.key == "ArrowDown"){
+    count = count + 50;
+    bird.style.bottom = count + "px";
+
+}
+if(e.key == "ArrowLeft"){
+    count = count + 50;
+    bird.style.left = count + "px";
+
+}
+  })
