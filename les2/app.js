91f2739805b03b4c11fc32328d39882375912b8f
LjaeYrosN <j2drxl@outlook.com>
2022-05-23 22:24:25 +0200

    m

--- les2/app.js
+++ les2/app.js
@@ -13,21 +13,25 @@ console.log(e);
 if(e.key == "ArrowRight"){
     count -= 50;
     bird.style.right = count + "px";
+    image.src = "img/b-right.svg";
 
 }
 if(e.key == "ArrowUp"){
     count = count + 50;
     bird.style.top = count + "px";
+    image.src = "img/b-up.svg";
 
 }
 if(e.key == "ArrowDown"){
     count = count + 50;
     bird.style.bottom = count + "px";
+    image.src = "img/b-down.svg";
 
 }
 if(e.key == "ArrowLeft"){
     count = count + 50;
     bird.style.left = count + "px";
+    image.src = "img/b-left.svg";
 
 }
   })
